426633152f8b26685445a3a26db4962df84da944
Rodrigo <rodrigo.silvadeoliveira324@gmail.com>
2025-05-28 20:30:15 -0300

    Caso usuário não tenha foto de perfil mostre um avatar padrão

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -5,7 +5,7 @@
         <div class="col-12 col-md-3 border-end">
             <div class="row border-bottom" style="background-color: #EEEFF2">
                 <div class="d-flex align-items-center justify-content-between">
-                    <img src="{{ auth()->user()->picture_url }}" class="img-profile rounded-circle" alt="img-profile">
+                    <img src="{{ auth()->user()->picture_url ?? asset('assets/imgs/avatar.png') }}" class="img-profile rounded-circle" alt="img-profile">
                     <p class="ms-4 fs-4"><strong>{{ auth()->user()->name }}</strong></p>
                     <button type="button" class="btn btn-danger" id="btn_logout">
                         <span class="material-symbols-outlined">logout</span>
